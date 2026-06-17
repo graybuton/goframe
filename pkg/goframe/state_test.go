676d1483787348d3619f5e466c0a42507b27f891
Buton Gray <graybuton@gmail.com>
2026-06-17 19:41:37 +0300

    test(runtime): cover reducer latest-state semantics

--- pkg/goframe/state_test.go
+++ pkg/goframe/state_test.go
@@ -179,6 +179,234 @@ func TestUnmountedStateDoesNotSchedule(t *testing.T) {
 	}
 }
 
+func TestUseReducerReturnsInitialState(t *testing.T) {
+	var value int
+	instance := testComponentInstance("Reducer", func() Node {
+		value, _ = UseReducer(3, func(state int, action int) int {
+			return state + action
+		})
+		return Empty()
+	}, nil)
+
+	renderComponentInstance(instance)
+
+	if value != 3 {
+		t.Fatalf("reducer initial state = %d, want 3", value)
+	}
+}
+
+func TestUseReducerDispatchAppliesToLatestState(t *testing.T) {
+	var value int
+	var dispatch func(int)
+	instance := testComponentInstance("Reducer", func() Node {
+		value, dispatch = UseReducer(0, func(state int, action int) int {
+			return state + action
+		})
+		return Empty()
+	}, nil)
+
+	renderComponentInstance(instance)
+	dispatch(1)
+	dispatch(1)
+	renderComponentInstance(instance)
+
+	if value != 2 {
+		t.Fatalf("reducer state = %d, want 2", value)
+	}
+}
+
+func TestUseReducerStaleDispatchUsesLatestState(t *testing.T) {
+	var value int
+	var firstDispatch func(int)
+	var latestDispatch func(int)
+	instance := testComponentInstance("Reducer", func() Node {
+		value, latestDispatch = UseReducer(0, func(state int, action int) int {
+			return state + action
+		})
+		if firstDispatch == nil {
+			firstDispatch = latestDispatch
+		}
+		return Empty()
+	}, nil)
+
+	renderComponentInstance(instance)
+	latestDispatch(5)
+	renderComponentInstance(instance)
+	firstDispatch(7)
+	renderComponentInstance(instance)
+
+	if value != 12 {
+		t.Fatalf("stale dispatch reducer state = %d, want 12", value)
+	}
+}
+
+func TestUseReducerStaleDispatchUsesLatestReducer(t *testing.T) {
+	var value int
+	var firstDispatch func(int)
+	var currentDispatch func(int)
+	useMultiplier := false
+	instance := testComponentInstance("Reducer", func() Node {
+		if useMultiplier {
+			value, currentDispatch = UseReducer(0, func(state int, action int) int {
+				return state + action*10
+			})
+		} else {
+			value, currentDispatch = UseReducer(0, func(state int, action int) int {
+				return state + action
+			})
+		}
+		if firstDispatch == nil {
+			firstDispatch = currentDispatch
+		}
+		return Empty()
+	}, nil)
+
+	renderComponentInstance(instance)
+	currentDispatch(1)
+	renderComponentInstance(instance)
+	useMultiplier = true
+	renderComponentInstance(instance)
+	firstDispatch(1)
+	renderComponentInstance(instance)
+
+	if value != 11 {
+		t.Fatalf("stale dispatch used wrong reducer, state = %d, want 11", value)
+	}
+}
+
+func TestUseReducerStateTypeMismatchPanics(t *testing.T) {
+	useString := false
+	instance := testComponentInstance("Reducer", func() Node {
+		if useString {
+			_, _ = UseReducer("", func(state string, action string) string {
+				return state + action
+			})
+		} else {
+			_, _ = UseReducer(0, func(state int, action int) int {
+				return state + action
+			})
+		}
+		return Empty()
+	}, nil)
+
+	renderComponentInstance(instance)
+	useString = true
+
+	assertPanic(t, "goframe: UseReducer state type changed between component renders", func() {
+		renderComponentInstance(instance)
+	})
+}
+
+func TestUseReducerReducerTypeMismatchPanics(t *testing.T) {
+	useStringAction := false
+	instance := testComponentInstance("Reducer", func() Node {
+		if useStringAction {
+			_, _ = UseReducer(0, func(state int, action string) int {
+				if action == "" {
+					return state
+				}
+				return state + 1
+			})
+		} else {
+			_, _ = UseReducer(0, func(state int, action int) int {
+				return state + action
+			})
+		}
+		return Empty()
+	}, nil)
+
+	renderComponentInstance(instance)
+	useStringAction = true
+
+	assertPanic(t, "goframe: UseReducer reducer type changed between component renders", func() {
+		renderComponentInstance(instance)
+	})
+}
+
+func TestUseReducerOutsideComponentPanics(t *testing.T) {
+	currentComponent = nil
+	assertPanic(t, "goframe: UseReducer must be called during component render", func() {
+		UseReducer(0, func(state int, action int) int {
+			return state + action
+		})
+	})
+}
+
+func TestUseReducerNilReducerPanics(t *testing.T) {
+	instance := testComponentInstance("Reducer", func() Node {
+		_, _ = UseReducer[int, int](0, nil)
+		return Empty()
+	}, nil)
+
+	assertPanic(t, "goframe: UseReducer reducer must not be nil", func() {
+		renderComponentInstance(instance)
+	})
+}
+
+func TestUseReducerDispatchAfterUnmountDoesNotSchedule(t *testing.T) {
+	var dispatch func(int)
+	reducerCalls := 0
+	schedules := 0
+	instance := testComponentInstance("Reducer", func() Node {
+		_, dispatch = UseReducer(0, func(state int, action int) int {
+			reducerCalls++
+			return state + action
+		})
+		return Empty()
+	}, func(*componentInstance) {
+		schedules++
+	})
+	renderComponentInstance(instance)
+	deactivateComponent(instance)
+
+	dispatch(1)
+
+	if schedules != 0 || reducerCalls != 0 || instance.dirty {
+		t.Fatalf("schedules=%d reducerCalls=%d dirty=%v, want inactive no-op", schedules, reducerCalls, instance.dirty)
+	}
+}
+
+func TestUseReducerDispatchDuringRenderRemainsDirty(t *testing.T) {
+	schedules := 0
+	instance := testComponentInstance("Reducer", func() Node {
+		value, dispatch := UseReducer(0, func(state int, action int) int {
+			return state + action
+		})
+		if value == 0 {
+			dispatch(1)
+		}
+		return Empty()
+	}, func(*componentInstance) {
+		schedules++
+	})
+
+	renderComponentInstance(instance)
+
+	if !instance.dirty || schedules != 1 {
+		t.Fatalf("dirty=%v schedules=%d, want dirty scheduled component", instance.dirty, schedules)
+	}
+}
+
+func TestUseReducerDispatchSamePrimitiveValueDoesNotSchedule(t *testing.T) {
+	var dispatch func(int)
+	schedules := 0
+	instance := testComponentInstance("Reducer", func() Node {
+		_, dispatch = UseReducer(1, func(state int, action int) int {
+			return state
+		})
+		return Empty()
+	}, func(*componentInstance) {
+		schedules++
+	})
+	renderComponentInstance(instance)
+
+	dispatch(1)
+
+	if instance.dirty || schedules != 0 {
+		t.Fatalf("dirty=%v schedules=%d, want no-op", instance.dirty, schedules)
+	}
+}
+
 func TestUpdateBatchCoalescesRequests(t *testing.T) {
 	var batch updateBatch
 	var queued []func()
@@ -210,4 +438,14 @@ func testComponentInstance(name string, render func() Node, schedule func(*compo
 		return render()
 	}).(ComponentNode)
 	return newComponentInstance(node, "", nil, schedule)
+}
+
+func assertPanic(t *testing.T, want any, fn func()) {
+	t.Helper()
+	defer func() {
+		if recovered := recover(); recovered != want {
+			t.Fatalf("panic = %v, want %v", recovered, want)
+		}
+	}()
+	fn()
 }
